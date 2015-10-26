5eabc9fc951a3e1e0252f29fcf45397977792144
Anand Gaurav <anangaur@users.noreply.github.com>
2015-10-26 15:18:43 +0530

    Update installcoffee.sh

--- CoffeeShop/Deploy/installcoffee.sh
+++ CoffeeShop/Deploy/installcoffee.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # update the apt-get repos
-#sudo apt-get -y update
+sudo apt-get -y update
 
 # Install the Java runtime
-#sudo apt-get -y install default-jre
+sudo apt-get -y install default-jre
 
 # Install Tomcat7
-#sudo apt-get -y install tomcat7
+sudo apt-get -y install tomcat7
 
 # Copy the path as 1st argument to coffee.war
-echo "Copying $1 as coffeeshop.war...."
-sudo cp $1 coffee.war
-echo "..done !"
+#echo "Copying $1 as coffeeshop.war...."
+#sudo cp $1 coffee.war
+#echo "..done !"
 
 # Copy the coffee.war to webapps folder
-echo "Copying coffeeshop.war to /var/lib/tomcat7/webapps/..."
-sudo cp coffeeshop.war /var/lib/tomcat7/webapps/
+echo "Copying $1 to /var/lib/tomcat7/webapps/..."
+sudo cp $1 /var/lib/tomcat7/webapps/
 echo "..done !"
