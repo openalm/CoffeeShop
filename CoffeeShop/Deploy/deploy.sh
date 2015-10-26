4d1d7ceeb6ffbdb745a6ff412027625ef378db2e
Anand Gaurav <anangaur@users.noreply.github.com>
2015-10-26 14:47:12 +0530

    Update deploy.sh

--- CoffeeShop/Deploy/deploy.sh
+++ CoffeeShop/Deploy/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# install Jarva runtime, if not present
 sudo apt-get -y install default-jre
+
+# install tomcat7, if not present
 sudo apt-get -y install tomcat7
+
 sudo cp TheCoffeeShop.war /var/lib/tomcat7/webapps/
